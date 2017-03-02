c5382e0f5ba9ffac6fd92f1f44c7ede86034b29a
kcarretto <kcarretto@gmail.com>
2017-03-02 01:05:39 -0500

    Added powershell script to assign roles + users to vms

---
+++ mass_addperms.ps1
@@ -0,0 +1,12 @@
+﻿$ROLE_NAME = "BlueTeam"
+
+$csv = Import-Csv -Path '.\team_users.csv' -Header name, teamid
+
+Foreach($user in $csv) {
+    $vms = Get-VM -name "Team $($user.teamid) *"
+    Foreach($vm in $vms) {
+        $role = Get-VIRole -Name "$ROLE_NAME"
+        Write-Host "Assigning Role $($role) to: $($vm.Name)"
+        New-VIPermission -Entity ($vm) -Role ($role) -Principal "WHITETEAM.VSPHERE\$($user.name)"
+    }
+}
