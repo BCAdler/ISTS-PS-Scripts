e14c7260e2c4c8772330f78b95ddac0dfa4cadd1
Brandon Adler <bcadler1@gmail.com>
2017-03-01 22:54:02 -0500

    I don't know what I did

---
+++ Virtual Infrastructure Deployment/vAppCreation.ps1
@@ -0,0 +1,22 @@
+Import-Module -Name VMware.VimAutomation.Core
+
+function New-TeamFolders {
+    for ($i = 0; $i -lt 11; $i++) {
+        (Get-View (Get-View -viewtype datacenter -filter @{"name"="WhiteTeam"}).vmfolder).CreateFolder("Team$i")
+    }
+}
+
+function Move-TeamVMs {
+    for ($i = 0; $i -lt 11; $i++) {
+        $folder = (Get-View -viewtype folder -Filter @{"name"="Team$i"})
+        Get-VM -Name "Team $i*" | Move-VM -Destination $folder[$i]
+    }
+}
+
+function New-TeamVApps {
+    for ($i = 0; $i -lt 11; $i++) {
+        New-VApp 
+    }
+}
+
+    
