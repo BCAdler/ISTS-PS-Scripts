b805ce6625bdd8a91f13e46f8e95cc1fa93312f5
Brandon Adler <bcadler1@gmail.com>
2017-06-05 23:57:21 -0400

    Starting to create Hailmary module to fully automate infrastructure deployment.

--- ISTS-Hailmary.psm1
+++ ISTS-Hailmary.psm1
@@ -1,3 +1,41 @@
 <#
     Module used to fully deploy the ISTS infrastructure from start to finish using the YAML config.
+    If there are any errors in the steps below, execution stops as it cannot continue without the previous step.
+
+    Steps:
+        1:  Import YAML config using Import-ISTSYAMLConfig
+        2:  Connect to vCenter using settings from config.
+        3:  Create team networks/portgroups on Competition Distributed Switch
+        4:  Create team folders that will show up in "VMs and Templates"
+        5:  Create team accounts on vCenter that they can use to access their infrastructure.
 #>
+
+function Start-Hailmary {
+    Param (
+        # Parameters will be added as necessary to keep up-to-date with the steps above during development.
+        [string]$ConfigFile = "$($ISTS_ModulePath)\ISTS-Config.yaml"
+    )
+    # Initial Setup
+
+    # Get array of team numbers
+    $TeamNumbers = 0..$ISTS.config.NumberOfTeams
+
+    # Numbers below correspond to the steps as outlined above
+    # 1
+    Import-ISTSYAMLConfig -ConfigFile $ConfigFile -ErrorAction Stop
+
+    # 2
+    $vCenterPass = ConvertTo-SecureString -String $ISTS.vcenter.password -AsPlainText -Force
+    $vCenterCred = New-Object System.Management.Automation.PSCredential($ISTS.vcenter.username, $vCenterPass)
+    Connect-VIServer -Server $ISTS.vcenter.address -Credential $vCenterCred -ErrorAction Stop
+
+    # 3
+    Add-ISTSNetworks -TeamNumbers $TeamNumbers
+
+    # 4
+    # Make parent folder for teams
+    $ParentFolder = New-Folder -Name "Team Folders" -Location Get-Datacenter[0]
+    Add-ISTSVMFolders -TeamNumbers $TeamNumbers -ParentFolder $ParentFolder
+
+    #5 - Must create this function.
+}
