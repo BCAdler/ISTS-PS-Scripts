b52ea8cbf2e699ced1cd3b6fc0df06560d9f2595
Brandon Adler <bcadler1@gmail.com>
2017-06-06 21:35:18 -0400

    Edited Start-VAppDeployment to use YAML config.

--- VMware-Deployment.psm1
+++ VMware-Deployment.psm1
@@ -33,80 +33,87 @@
 function Start-VAppDeployment {
     Param (
         [Parameter(Mandatory=$true)][int[]]$TeamNumbers,
-        [Parameter(Mandatory=$true)][string]$TemplateVAppName,
+        [string]$TemplateVAppName = $ISTS.Config.TemplateVApp,
         [string]$OverrideDatastore,
-        [switch]$StartVApp,
-        [string]$PathToTeamNetworkCsv = "$ISTS_ModulePath/vlan_info_final.csv"
+        [switch]$StartVApp
     )
+    # Time how long it takes to deploy a team
     $StartTime = Get-Date
 
-    # Get Team Network CSV 
-    $TeamNetworksCsv = Import-Csv $PathToTeamNetworkCsv
-    $ProdVMs = "Web","DB","Win8"
-    $CorpVMs = "AD","Mail","Kali","Parrot","VOIP"
-
-    # Get vApp object from given name
+    # Initial Config Variables
+    # Get Team Networks 
+    $TeamNetworks = $ISTS.NetworkConfig.Networks.Keys
+    # Get template vApp object from its name
     $TemplateVApp = Get-VApp -Name $TemplateVAppName
+
+    # Get Host Cluster or single host
     $Location = Get-Cluster
     if($Location -eq $null) {
         $Location = $(Get-VMHost)[0]
     }
     
-    foreach($i in $TeamNumbers) {
-        # Get Datastore Cluster or Datastore with most free space.        
-        $Datastores = Get-Datastore
-        $Datastore = $Datastores[0]
-        foreach($store in $Datastores) {
-            if($store.FreeSpaceGB -gt $Datastore.FreeSpaceGB) {
-                $Datastore = $store
+    foreach($TeamNumber in $TeamNumbers) {
+        # Get Datastore Cluster or Datastore with most free space.  
+        $Datastore = Get-DatastoreCluster
+        if($Datastore -eq $null) {
+            $Datastores = Get-Datastore
+            $Datastore = $Datastores[0]
+            foreach($store in $Datastores) {
+                if($store.FreeSpaceGB -gt $Datastore.FreeSpaceGB) {
+                    $Datastore = $store
+                }
             }
         }
 
-        if((Get-VApp -Name "Team $i") -eq $null) {
+        $VAppName = $ISTS.Templates.vAppName.Replace('$TeamNumber', $TeamNumber)
+        if((Get-VApp -Name $VAppName) -eq $null) {
             # Start cloning of template vApp and wait until it is done to continue.
-            Write-Host "Starting Team $i vApp Cloning..." -ForegroundColor Yellow
-            $CloneTask = New-VApp -Name "Team $i" -Location $Location -VApp $TemplateVApp -Datastore $Datastore
+            Write-Host "Starting $VAppName vApp Cloning..." -ForegroundColor Yellow
+            $CloneTask = New-VApp -Name $VAppName -Location $Location -VApp $TemplateVApp -Datastore $Datastore
             if(!$?) {
                 Write-Host "Cloning vApp Failed..." -ForegroundColor Red -BackgroundColor Black
                 exit
             }
             Wait-Task -Task $CloneTask
-            Write-Host "Team $i vApp Cloning Complete" -ForegroundColor Green
+            Write-Host "$VAppName vApp Cloning Complete" -ForegroundColor Green
         }
         else {
-            Write-Host "vApp `"Team $i`" Already Exists...  Continuing" -ForegroundColor Yellow
+            Write-Host "vApp `"$VAppName`" Already Exists...  Continuing" -ForegroundColor Yellow
         }
         
-        Write-Host "`nRenaming and Configuring Networking for Team $i's VMs." -ForegroundColor Yellow
+        Write-Host "`nRenaming and Configuring Networking for $VAppName's VMs." -ForegroundColor Yellow
 
         # Get all VMs in the new vApp
-        $vms = Get-VApp -Name "Team $i" | Get-VM
-        foreach($vm in $vms) {
-            if($CorpVMs -contains $vm.Name.Split('-').Trim()[1]) {
-                $PortGroup = Get-VDPortGroup -Name ($TeamNetworksCsv.Purpose -like "Team $i Corp*")
-            }
-            elseif($ProdVMs -contains $vm.Name.Split('-').Trim()[1]) {
-                $PortGroup = Get-VDPortGroup -Name ($TeamNetworksCsv.Purpose -like "Team $i Prod*")
+        $VMs = Get-VApp -Name $VAppName | Get-VM
+
+        # For all VMs in new vApp, assign each to their respective team's network
+        foreach($VM in $VMs) {
+            foreach($TeamNetwork in $TeamNetworks) {
+                $Net = $ISTS.NetworkConfig.Networks.$TeamNetwork
+                if($Net.VMs -contains $VM.Name.Split('-').Trim()[1]) {
+                    # Get network name template from config and input variables.  Using * at end to avoid VLAN calculation which isn't necesary here.
+                    $PortGroupName = "$($ISTS.Templates.NetworkName.Replace('$TeamNumber', $TeamNumber).Replace('$Network', $TeamNetwork))*"
+                    $PortGroup = Get-VDPortGroup -Name $PortGroupName
+                    continue
+                }
             }
 
             # Change VM port group
-            $vm | Get-NetworkAdapter | Set-NetworkAdapter -Portgroup $PortGroup -Confirm:$false
+            $VM | Get-NetworkAdapter | Set-NetworkAdapter -Portgroup $PortGroup -Confirm:$false
 
             # Change VM name
-            Set-VM -VM $vm -Name "Team $i - $($vm.Name.Split('-').Trim()[1])" -Confirm:$false
+            $VMName = $ISTS.Templates.VMName.Replace('$TeamNumber', $TeamNumber).Replace('$VMName', $VM.Name.Split('-').Trim()[1])
+            Set-VM -VM $VM -Name $VMName -Confirm:$false
         }
         
         # Start new vApp if $StartVApp switch is $true
         if($StartVApp) {
-            Start-VApp -VApp (Get-VApp -Name "Team $i") -RunAsync
-            Write-Host "Team $i vApp starting." -ForegroundColor Yellow
+            Start-VApp -VApp (Get-VApp -Name $VAppName) -RunAsync
+            Write-Host "$VAppName vApp starting." -ForegroundColor Yellow
         }
-        Write-Host "Configuration for Team $i complete!" -ForegroundColor Green
-        Write-Host "Script Start: $StartTime"
-        Write-Host "Script End: $(Get-Date)"
-
-        #$vms=Get-VM
-        #$sw=Get-VirtualPortGroup
+        Write-Host "Configuration for $VAppName complete!" -ForegroundColor Green
+        Write-Host "Script Start: $StartTime" -ForegroundColor Green
+        Write-Host "Script End: $(Get-Date)" -ForegroundColor Green
     }
 }
 
@@ -238,55 +245,102 @@ function Add-Networks {
 
 <#
     .SYNOPSIS
-    Configures permissions for each team vApp.
+    Configures permissions for each team vApp.  Can be set to do folders and port groups as well.
 
     .DESCRIPTION
-    Configures permissions for each team vApp so users cannot see each others vApps or VMs.
+    Configures permissions for each team vApp so users cannot see each others vApps or VMs.  Can be set to do folders and port groups as well
 
     .PARAMETER TeamNumbers
     List of team numbers to congigure permissions for.
 
-    .PARAMETER RoleName # TODO: Make config variable for vCenter RoleName
+    .PARAMETER RoleName
     Name of the role used to assign permissions to teams.  Same name as configured in vCenter.
 
-    .PARAMETER DomainName # TODO: Make config variable for Domain Name
+    .PARAMETER DomainName
     Domain name used for the team accounts.  May be the vCenter SSO domain or the AD domain name depending on where the user accounts were created.
 
-    .PARAMETER CreateAccounts
-    NOT IMPLEMENTED YET.  Specifies whether to create accounts for the teams being deployed.
+    .PARAMETER FolderPermissions
+    Switch used to optionally set team permissions on their folder.
+
+    .PARAMETER FolderPermissions
+    Switch used to optionally set team permissions on their port groups.
 
     .EXAMPLE
     An example
 
     .NOTES
-    General notes
+    Functions to add team permissions to folders and port groups are added on as extra in the case of automation.
 #>
 function Add-TeamPermissions {
     Param (
         [Parameter(Mandatory=$true)][int[]]$TeamNumbers,
-        [string]$RoleName = "BlueTeam",
-        [string]$DomainName = "VSPHERE.LOCAL",
-        [Switch]$CreateAccounts = $false
+        [string]$RoleName = $ISTS.vCenter.Permissions.TeamRoleName,
+        [string]$DomainName = $ISTS.vCenter.SSO_Domain,
+        [switch]$FolderPermissions,
+        [switch]$PortGroupPermissions
     )
-    
-    if($CreateAccounts) {
-        Write-Host "Creating Team Accounts..." -ForegroundColor Yellow
-        Write-Host "Not Implemented Yet But Will Be Soon......" -ForegroundColor Yellow
-    }
+    # Initial Config Options
+    # Get domain name
+    $SSO_Domain = $ISTS.vCenter.SSO_Domain
 
-    foreach($i in $TeamNumbers) {
+    # Get role to be assigned
+    $Role = Get-VIRole -Name $RoleName
+    
+    # TODO: Doesn't differentiate between Corp and Prod.  AKA: User shouldn't have access to Prod VMs directly.
+    foreach($TeamNumber in $TeamNumbers) {
+        # Set permissions on vApps
+
         # Regex Meaning: vApp name with "Team $i" that matches the number and then anything after the number or nothing
         #                Fixes issues where "Team $i*" will match "Team 1" and "Team 10."
-        $vapps = Get-VApp | Where-Object { $_.Name -match "Team \b${i}\b(?:.*)?" }
+        $vApps = Get-VApp | Where-Object { $_.Name -match "Team \b${TeamNumber}\b(?:.*)?" }
 
-        foreach($vapp in $vapps) {
-            $role = Get-VIRole -Name "$RoleName"
-            Write-Host "Assigning User: Team$i with Role: $role to: $($vapp.Name)"
-            New-VIPermission -Entity $vapp -Role $role -Principal "$DomainName\team$i"
+        # TODO: Only get vApp(s) the teams should have access to.  Must edit above statement
+
+        
+        $Username = $ISTS.Templates.Username.Replace('$TeamNumber', $TeamNumber)
+        foreach($vApp in $vApps) {
+            Write-Host "Assigning User: $SSO_Domain\$Username with Role: $Role to: $($vApp.Name)"
+            New-VIPermission -Entity $vApp -Role $Role -Principal "$SSO_Domain\$Username"
+        }
+
+        # Optionally set permissions on folders
+        if($FolderPermissions) {
+            $FolderName = $ISTS.Templates.FolderName.Replace('$TeamNumber', $TeamNumber)
+            $Folder = Get-Folder -Name $FolderName
+            New-VIPermission -Entity $Folder -Role $Role -Principal $Username
+        }
+
+        # Optionally set permissions on port groups
+        if($PortGroupPermissions) {
+
         }
     }
 }
 
-# TODO: Make Add-TeamAccounts to add accounts for all of the teams into vCenter.
+<#
+    .SYNOPSIS
+    Adds team accounts to vCenter so users can log into their environment.
+
+    .DESCRIPTION
+    Adds team accounts to vCenter so users can log into their environment.
+
+    .PARAMETER TeamNumbers
+    List of team numbers to configure permissions for.
+
+    .EXAMPLE
+    Add-TeamAccounts -TeamNumbers 1,2,3,4,5,6,7,8,9,10
+
+    .NOTES
+    Uses YAML config to retrieve user properties such as Username and password.
+#>
+function Add-TeamAccounts {
+    Param (
+        [Parameter(Mandatory=$true)][int[]]$TeamNumbers
+    )
+    foreach($TeamNumber in $TeamNumbers) {
+        $NewAccountName = $ISTS.Templates.Username.Replace('$TeamNumber', $TeamNumber)
+        New-VMHostAccount -Id $NewAccountName -Password $ISTS.vCenter.Permissions.TeamPassword -Description "Team $TeamNumber's Account"
+    }
+}
 
 # TODO: Make Add-Host function to add a new host to vCenter and fully configure it
